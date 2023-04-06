c74932a9f8e6eec4e4f51b962bdb15a8ee282d5d
Rogen Barrow <rogenbarrow@gmail.com>
2023-04-06 16:00:43 -0400

    adjustment

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 5055,
-  mockServer: process.env.MOCK_SERVER || "http://localhost:5056",
+  mockServer: process.env.MOCK_SERVER || "http://10.10.210.119:5055",
   alchemyBaseUrl: process.env.ALCHEMY_BASE_URL || "http://localhost",
 };
 
