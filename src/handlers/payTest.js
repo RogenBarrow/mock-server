9566f761d2fed3e9496a8a57bbc92dd34bb4872e
Rogen Barrow <rogenbarrow@gmail.com>
2023-04-14 10:45:55 -0400

    edit payments

--- src/handlers/payTest.js
+++ src/handlers/payTest.js
@@ -31,7 +31,7 @@ const payTest = async (req, res) => {
     // Post the XML data to the server
     console.log(alchemyBaseUrl);
     const result = await axios.post(
-      `${alchemyBaseUrl}/request/reqtype/2`,
+      `${alchemyBaseUrl}/request/reqtype/1`,
       { data: xmlData },
       config
     );
