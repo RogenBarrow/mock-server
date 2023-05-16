b95d0f6a036cc2071a90998b251b8d065e6f3334
Rogen Barrow <rogenbarrow@gmail.com>
2023-05-16 15:26:02 -0400

    small edit in the xml

--- src/handlers/payTest.js
+++ src/handlers/payTest.js
@@ -41,7 +41,7 @@ const payTest = async (req, res) => {
     convertedData.request.tranid = id;
 
     // Convert the updated JSON object back to XML
-    const updatedXML = `<?xml version="1.0" encoding="UTF-8"?>\n${builder.build(convertedData)}`;
+    const updatedXML = `<?xml version="1.0" encoding="UTF-8"?>${builder.build(convertedData)}`;
     console.log("updated:", updatedXML);
 
     // Post the XML data to the server
