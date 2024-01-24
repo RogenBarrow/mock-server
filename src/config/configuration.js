00fc31f7bc5dbaa792c1a910dd24932512f9ed0b
Rogen Barrow <rogenbarrow@gmail.com>
2024-01-24 14:57:57 -0400

    change connection config

--- src/config/configuration.js
+++ src/config/configuration.js
@@ -1,7 +1,7 @@
 const config = {
   port: process.env.PORT || 5055,
   mockServer: process.env.MOCK_SERVER || "http://10.10.210.119:5055",
-  alchemyBaseUrl: process.env.ALCHEMY_BASE_URL || "http://localhost",
+  alchemyBaseUrl: process.env.ALCHEMY_BASE_URL || "http://192.168.100.151:97",
 };
 
 module.exports = config;
